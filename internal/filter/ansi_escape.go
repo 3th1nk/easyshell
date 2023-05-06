506568767583b5b3e25b34a0c0b24cbcbda0feff
tantao <343389789@qq.com>
2023-05-06 19:02:35 +0800

    perf: 优化ANSI Escape字符过滤逻辑

---
+++ internal/filter/ansi_escape.go
@@ -0,0 +1,71 @@
+package filter
+
+// checkAnsiEscape 检查控制字符
+//	参考资料：
+//	https://vt100.net/docs/vt100-ug/chapter3.html#S3.3
+//	https://en.wikipedia.org/wiki/ANSI_escape_code#CSI_(Control_Sequence_Introducer)_sequences
+// TODO 对于修改内容的控制符，应该同时处理对应的内容
+// 	由于当前函数是在每次读操作之后调用，涉及修改的内容可能还未被完整的读取到缓存区，可能导致异常
+func checkAnsiEscape(s []byte, pos int) (bool, int, [2]int) {
+	length := len(s)
+	if pos >= length || s[pos] != '\x1b' {
+		return false, pos, [2]int{}
+	}
+	start := pos
+	// skip '\x1b'
+	pos++
+
+	if pos < length {
+		switch s[pos] {
+		case 'N':
+			// SS2 (Single Shift Two)
+		case 'O':
+			// SS3 (Single Shift Three)
+		case 'P':
+			// DCS (Device Control String)
+		case '[':
+			// CSI (Control Sequence Introducer)
+			// TODO: support 24-bit colors, such as `ESC[38;2;⟨r⟩;⟨g⟩;⟨b⟩ m`
+
+			// skip '['
+			pos++
+
+			//	any number (including none) of "parameter bytes" in the range 0x30–0x3F (ASCII 0–9:;<=>?)
+			for pos < length {
+				if s[pos] < '\x30' || s[pos] > '\x3F' {
+					break
+				}
+				pos++
+			}
+
+			//	then any number (including none) of "intermediate bytes" in the range 0x20–0x2F (ASCII space and !"#$%&'()*+,-./)
+			for pos < length {
+				if s[pos] < '\x20' || s[pos] > '\x2F' {
+					break
+				}
+				pos++
+			}
+
+			//	finally by a single "final byte" in the range 0x40–0x7E (ASCII @A–Z[\]^_`a–z{|}~)
+			if pos < length && s[pos] >= '\x40' && s[pos] <= '\x7E' {
+				pos++
+				return true, pos, [2]int{start, pos}
+			}
+
+		case '\\':
+			// ST (String Terminator)
+		case ']':
+			// OSC	(Operating System Command)
+		case 'X':
+			// SOS	(Start of String)
+		case '^':
+			// PM (Privacy Message)
+		case '_':
+			// APC (Application Program Command)
+		case 'c':
+			// RIS (Reset to Initial State)
+		}
+	}
+
+	return false, pos, [2]int{}
+}
--- internal/filter/filter_test.go
+++ internal/filter/filter_test.go
@@ -8,7 +8,7 @@ import (
 var (
 	ctrlStrArr = [][]string{
 		{"abcc\b", "abc"},
-		{"\babcc\bc\b", "abc"},
+		{"\babcc\bcc\b\b", "abc"},
 		{"\x1b[Kabc", "abc"},
 		{"\x1b[2habc", "abc"},
 		{"\x1b[?25labc", "abc"},
@@ -19,14 +19,15 @@ var (
 		{"\x1b[31mabc\x1b[0m", "abc"},
 		{"\x1b[31;47mabc\x1b[0m", "abc"},
 		{"\x1b[31:47mabc\x1b[0m", "abc"},
-		{"\x1b[31;47;<r>;<g>;<b>mabc\x1b[0m", "abc"},
+		//{"\x1b[31;47;<r>;<g>;<b>mabc\x1b[0m", "abc"},
 		{"\u001B[80;1HCD_DA11F_MX01#\x1b[80;1H\x1b[80;17H", "CD_DA11F_MX01#"},
+		{"\u001B[1;13r\u001B[1;1H\u001B[80;1HPress any key to continue\n\u001B[13;1H\u001B[?25h\u001B[80;27H\u001B[?6l\u001B[1;80r\u001B[?7h\u001B[2J\u001B[1;1H\u001B[1920;1920H\u001B[6n\u001B[1;1HYour previous successful login (as manager) was on 2022-08-25 01:30:16 from 172.26.66.11 \u001B[1;80r\u001B[80;1H\u001B[80;1H\u001B[2K\u001B[80;1H\u001B[?25h\u001B[80;1H\u001B[80;1HCD_OA_11F_MX01#\u001B[80;1H\u001B[80;17H\u001B[80;1H\u001B[?25h\u001B[80;17H", "Press any key to continue\nYour previous successful login (as manager) was on 2022-08-25 01:30:16 from 172.26.66.11 CD_OA_11F_MX01#"},
 	}
 )
 
 func TestCtrlFilter(t *testing.T) {
 	for _, val := range ctrlStrArr {
-		assert.Equal(t, CtrlFilter([]byte(val[0])), []byte(val[1]))
+		assert.Equal(t, []byte(val[1]), CtrlFilter([]byte(val[0])))
 	}
 }
 
