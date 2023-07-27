7a2467c288295f84086f7004f65ec4b8738c911e
tantao <343389789@qq.com>
2023-07-27 12:15:40 +0800

    fix

--- pkg/reader/reader_test.go
+++ pkg/reader/reader_test.go
@@ -38,7 +38,7 @@ func TestDefaultEndPromptRegex(t *testing.T) {
 }
 
 func Test_findEndPromptRegexp(t *testing.T) {
-	promptStr := "HA-备 #"
+	promptStr := "root@HA-备_试试长一点的中文 #"
 	re := findEndPromptRegexp(promptStr)
 	t.Log(re.String())
 	assert.Equal(t, true, re.MatchString(promptStr))
