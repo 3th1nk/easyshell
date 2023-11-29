c89357929667daf99d57dcb0e0e90c358da24cc9
tantao <343389789@qq.com>
2023-11-29 18:29:29 +0800

    update

--- pkg/interceptor/answer.go
+++ pkg/interceptor/answer.go
@@ -3,9 +3,9 @@ package interceptor
 import "regexp"
 
 func AlwaysYes() Interceptor {
-	return Regexp(regexp.MustCompile(`(?i)[\[(](y|yes)[/|]?(n|no)[])][:?]\s*$`), "y", lastLine, false)
+	return Regexp(regexp.MustCompile(`(?i)[\[(](y|yes)[/|]?(n|no)[])][:?]\s*$`), "y", LastLine, false)
 }
 
 func AlwaysNo() Interceptor {
-	return Regexp(regexp.MustCompile(`(?i)[\[(](y|yes)[/|]?(n|no)[])][:?]\s*$`), "n", lastLine, false)
+	return Regexp(regexp.MustCompile(`(?i)[\[(](y|yes)[/|]?(n|no)[])][:?]\s*$`), "n", LastLine, false)
 }
--- pkg/interceptor/continue.go
+++ pkg/interceptor/continue.go
@@ -3,5 +3,5 @@ package interceptor
 import "regexp"
 
 func Continue() Interceptor {
-	return Regexp(regexp.MustCompile(`(?i)^press\s+any\s+key\s+to\s+continue`), " ", lastLine, false)
+	return Regexp(regexp.MustCompile(`(?i)^press\s+any\s+key\s+to\s+continue`), " ", LastLine, false)
 }
