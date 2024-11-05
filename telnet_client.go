760bf60fcbb32ce7129a70efb004b8459f5733e1
tantao <343389789@qq.com>
2024-11-05 15:04:15 +0800

    perf:优化代码

--- telnet_client.go
+++ telnet_client.go
@@ -8,19 +8,22 @@ import (
 )
 
 type TelnetCredential struct {
-	Host     string `json:"host"`               // IP地址
-	Port     int    `json:"port,omitempty"`     // 端口，默认23
-	User     string `json:"user,omitempty"`     // 用户名，可选
-	Password string `json:"password,omitempty"` // 密码，可选
-	Timeout  int    `json:"timeout,omitempty"`  // 连接超时时间（秒），默认15秒
+	Host     string        `json:"host"`               // IP地址
+	Port     int           `json:"port,omitempty"`     // 端口，默认23
+	User     string        `json:"user,omitempty"`     // 用户名，可选
+	Password string        `json:"password,omitempty"` // 密码，可选
+	Timeout  time.Duration `json:"timeout,omitempty"`  // 连接超时时间，默认15秒
 }
 
 func NewTelnetClient(cred *TelnetCredential) (*telnet.Client, error) {
-	timeout := util.IfEmptyInt(cred.Timeout, 15)
+	timeout := cred.Timeout
+	if timeout <= 0 {
+		timeout = 15 * time.Second
+	}
 	return telnet.NewClient(&telnet.ClientConfig{
 		Addr:     fmt.Sprintf("%s:%d", cred.Host, util.IfEmptyInt(cred.Port, 23)),
 		User:     cred.User,
 		Password: cred.Password,
-		Timeout:  time.Duration(timeout) * time.Second,
+		Timeout:  timeout,
 	})
 }
